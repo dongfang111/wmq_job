4305d6a6606ab6c01f842a63309919d8fb5a8d6b
tianfeng <tf7353@163.com>
2021-03-23 20:58:39 +0800

    简单链表算法提交

---
+++ demo/src/main/java/com/example/demo/agorithm/DeleteDuplicates.java
@@ -0,0 +1,18 @@
+package com.example.demo.agorithm;
+
+import com.example.demo.agorithm.reverse.ListNode;
+
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ *
+ * @author wmqing
+ * @Date 2021/3/23 20:53
+ **/
+public class DeleteDuplicates {
+
+    public static void deleteDuplicates(ListNode head) {
+
+    }
+}
