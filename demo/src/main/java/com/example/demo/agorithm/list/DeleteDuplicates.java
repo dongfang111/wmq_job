62a39decb521258e015e5e447f4fb1c393b43d4c
zhuyp <zhuyp@ziroom.com>
2021-04-05 23:32:37 +0800

    JAVA系统学习整理

--- demo/src/main/java/com/example/demo/agorithm/list/DeleteDuplicates.java
+++ demo/src/main/java/com/example/demo/agorithm/list/DeleteDuplicates.java
@@ -2,6 +2,8 @@ package com.example.demo.agorithm.list;
 
 import com.example.demo.agorithm.list.reverse.ListNode;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
  * 输入: 1->1->2->3->3
@@ -25,19 +27,38 @@ public class DeleteDuplicates {
     }
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(1);
-        l1.next.next = new ListNode(1);
-        l1.next.next.next = new ListNode(3);
-        l1.next.next.next.next = new ListNode(3);
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
+//        threadPoolExecutor.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+        try {
+            ListNode l1 = new ListNode(1);
+            l1.next = new ListNode(1);
+            l1.next.next = new ListNode(1);
+            l1.next.next.next = new ListNode(3);
+            l1.next.next.next.next = new ListNode(3);
 
-        ListNode listNode = deleteDuplicates(l1);
-        ListNode p = listNode;
+//            new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//
+//                }
+//            }).start();
+
+            ListNode listNode = deleteDuplicates(l1);
+            ListNode p = listNode;
 //        p.next = null;
 //        listNode.next = null;
-        while (p != null) {
-            System.out.println(p.val);
-            p = p.next;
+            while (p != null) {
+                System.out.println(p.val);
+                p = p.next;
+            }
+            throw new RuntimeException();
+        } finally {
+            System.out.println("finally");
         }
     }
 }
